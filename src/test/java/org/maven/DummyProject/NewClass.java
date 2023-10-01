56c18e973e868abc4947832a4806266a05979892
AdityaB <adityarangab@gmail.com>
2023-10-01 17:18:08 +0530

    Commit made in develop branch in GitStuff Workspace

---
+++ src/test/java/org/maven/DummyProject/NewClass.java
@@ -0,0 +1,13 @@
+package org.maven.DummyProject;
+
+import org.testng.annotations.Test;
+
+public class NewClass {
+
+	
+	@Test
+	public void method() {
+		System.out.println("Java programming language");
+	}
+	
+}
