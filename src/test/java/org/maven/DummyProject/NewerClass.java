cb9106b084ae284817d3f70e3815f573c66a39b7
AdityaB <adityabranga@gmail.com>
2023-10-01 19:10:26 +0530

    Commit made on develop branch by PracticeRepo

---
+++ src/test/java/org/maven/DummyProject/NewerClass.java
@@ -0,0 +1,13 @@
+package org.maven.DummyProject;
+
+import org.testng.annotations.Test;
+
+public class NewerClass {
+	
+	
+	@Test
+	public void method() {
+		System.out.println("Commit made to develop branch by PracticeRepo");
+	}
+
+}
