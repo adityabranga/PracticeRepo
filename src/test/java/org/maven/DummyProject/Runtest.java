51718863e4b241c3fbed7c7f9e7c614bbafe0e7c
AdityaB <adityarangab@gmail.com>
2023-10-02 21:03:46 +0530

    Gitstuff develop1 branch commit

--- src/test/java/org/maven/DummyProject/Runtest.java
+++ src/test/java/org/maven/DummyProject/Runtest.java
@@ -8,19 +8,13 @@ public class Runtest {
 	@Test
 	public void method() {
 		System.out.println("java");
+		
 	}
 
 	
-	@Test
-	public void NewMethod() {
-		System.out.println("programming");
-	}
 	
 	
-	@Test
-	public void main() {
-		System.out.println("Hello world");
-	}
+	
 	
 	
 }
